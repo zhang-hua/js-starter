f78de40862b32507a572ea44b5865686fafd56e4
Zhang Hua <huazhan@microsoft.com>
2022-08-04 14:00:58 -0700

    Add script to test insert and query on random vectors

---
+++ index.js
@@ -0,0 +1,163 @@
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(uri);
+const db = client.db('test');
+const insertresults = db.collection('insert_results');
+const queryresults = db.collection('query_results');
+
+function getCollection(bulkSize, randomVector) {
+    return db.collection(`vectors_${bulkSize}_${randomVector}`);
+}
+
+var randomVector = function(size) {
+    var v = [];
+    for(var i = 1; i <= size; i++)
+    {
+        v.push(Math.random())
+    }
+    return v;
+}
+
+// Create a collection and insert 'vectorCount' vectors
+async function bulkInsertVectors(collection, vectorSize, vectorCount, bulkSize, randomVectorCount)
+{
+    if (vectorCount < randomVectorCount)
+    {
+        var vectorCache =[];
+        for (var i = 0; i < randomVectorCount; i++)
+        {
+            vectorCache.push(randomVector(vectorSize));
+        }
+        var bulk = [];
+        for (var i = 1; i <= vectorCount; i++) {
+            bulk.push({ "_id" : i, "value": vectorCache[i % randomVectorCount]});
+            if (bulk.length == bulkSize) {
+                await collection.insertMany(bulk);
+                bulk = [];
+            }
+        }
+    }
+    else
+    {
+        for (var i = 1; i <= vectorCount; i++) {
+            await collection.insertOne({ "_id" : i, "value": randomVector(vectorSize)});
+        }
+    }
+}
+
+async function testBulkInsertion(vectorSize, vectorCount, bulkSizeSpace, randomVectorCountSpace)
+{
+    for (var i = 0; i < bulkSizeSpace.length; i++)
+    {
+        for (var j = 0; j < randomVectorCountSpace.length; j++)
+        {
+            var collection = getCollection(bulkSizeSpace[i], randomVectorCountSpace[j]);
+            const collections = await db.listCollections().toArray()
+            if (collections.find(c => c.name == collection.collectionName))
+            {
+                await collection.drop();
+            }
+            var randomVectorCount = Math.min(vectorSize, randomVectorCountSpace[j]);
+            var bulkSize = Math.min(vectorSize, bulkSizeSpace[i]);
+            var startTime = new Date();
+            await bulkInsertVectors(collection, vectorSize, vectorCount, bulkSize, randomVectorCount);
+            var endTime = new Date();
+            await insertresults.insertOne({"collectionName": collection.collectionName, "vectorSize": vectorSize, "vectorCount": vectorCount, "bulkSize": bulkSize, "randomVectorCount": randomVectorCount, "startTime": startTime, "endTime": endTime, "durationMs": endTime - startTime });
+        }
+    }
+}
+
+async function queryCollection(collection, sorting, top)
+{
+    var result = [];
+    if (sorting)
+    {
+        result = await collection.aggregate(
+            [
+                {
+                    $project: {
+                        cosine: {
+                            $reduce: {
+                                input: { $range: [ 0, { $size: "$value" }] },
+                                initialValue: 0,
+                                in: { $add: [ "$$value", { $multiply: [ { $arrayElemAt: [ "$value", "$$this" ] }, { $arrayElemAt: [ "$$inputVector", "$$this" ] } ] } ] }
+                            }
+                        }
+                    }
+                },
+                {  
+                    $sort: { cosine: -1 }
+                },
+                {   $limit: top   }
+            ],
+            { let: { inputVector: randomVector(1024) } }
+        ).toArray();
+    }
+    else
+    {
+        result = await collection.aggregate(
+            [
+                {
+                    $project: {
+                        cosine: {
+                            $reduce: {
+                                input: { $range: [ 0, { $size: "$value" }] },
+                                initialValue: 0,
+                                in: { $add: [ "$$value", { $multiply: [ { $arrayElemAt: [ "$value", "$$this" ] }, { $arrayElemAt: [ "$$inputVector", "$$this" ] } ] } ] }
+                            }
+                        }
+                    }
+                },
+                {   $limit: top   }
+            ],
+            { let: { inputVector: randomVector(1024) } }
+        ).toArray();
+    }
+    return result;
+}
+
+async function testQuery(bulkSizeSpace, randomVectorCountSpace, sortingSpace, topSpace)
+{
+    for (var i = 0; i < bulkSizeSpace.length; i++)
+    {
+        for (var j = 0; j < randomVectorCountSpace.length; j++)
+        {
+            var collection = getCollection(bulkSizeSpace[i], randomVectorCountSpace[j]);
+            for (var k = 0; k < sortingSpace.length; k++)
+            {
+                for (var l = 0; l < topSpace.length; l++)
+                {
+                    var startTime = new Date();
+                    var result = await queryCollection(collection, sortingSpace[k], topSpace[l]);
+                    var endTime = new Date();
+                    await queryresults.insertOne({"collectionName": collection.collectionName, "bulkSize": bulkSizeSpace[i], "randomVectorCount": randomVectorCountSpace[j], "sorting": sortingSpace[k], "top": topSpace[l],  "startTime": startTime, "endTime": endTime, "durationMs": endTime - startTime, "resultCount": result.length });
+                }
+            }
+        }
+    }
+}
+
+async function main()  {
+    try
+    {
+        var bulkSizeSpace = [1, 10, 100, 1000, 10000];
+        var randomVectorCountSpace = [1, 10, 100, 1000, 100000];
+        var sortingSpace = [true, false];
+        var topSpace = [10, 100, 1000];
+        var vectorSize = 1024;
+        var vectorCount = 100000;
+        await testBulkInsertion(vectorSize, vectorCount, bulkSizeSpace, randomVectorCountSpace);
+        await testQuery(bulkSizeSpace, randomVectorCountSpace, sortingSpace, topSpace);
+    }
+    catch (error)
+    {
+        console.error(error);
+    }
+    finally
+    {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+main();
